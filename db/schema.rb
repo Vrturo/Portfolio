f6063766b1d26bd98490395bb7187cef3174016d
Arturo Perez <VrturoPerez@gmail.com>
2015-11-11 19:29:26 -0800

    replace sqlite3 with pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112010015) do
-
-  create_table "resumes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
