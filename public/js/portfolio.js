5a856c90437626d8746452dcabe374d8af1a005b
Arturo Perez <VrturoPerez@gmail.com>
2016-03-04 15:34:44 -0800

    add smooth scrolling to links

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
-  console.log("this");
   $("#aboutLink").click(function(e) {
       e.preventDefault();
       $('html, body').animate({
           scrollTop: $("#about").offset().top
+      }, 1500);
+  });
+
+  $("#projectsLink").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $("#projects").offset().top
+      }, 1500);
+  });
+
+  $("#contactLink").click(function(e) {
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $("#contact").offset().top
       }, 1500);
   });
 })
