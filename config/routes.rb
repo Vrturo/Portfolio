6da98158fe7df0bccdc3c049f3deb0a3c4ba4d60
Arturo Perez <VrturoPerez@gmail.com>
2016-08-09 03:04:38 -0700

    add challenge route and files

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  get '/bitTorrentChallenge' => 'challenge#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
