f2fb18928e25ff7c2d0b8d8586d9083d75ae8d29
Arturo Perez <VrturoPerez@gmail.com>
2016-09-30 10:52:05 -0700

    add blog route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   get '/bitTorrentChallenge' => 'challenge#index'
 
+  get '/blog' => 'blog#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
