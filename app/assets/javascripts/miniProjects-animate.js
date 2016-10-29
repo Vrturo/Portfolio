a209110dcb69cfc4d969a010d19b5936d42a4eb9
Arturo Perez <VrturoPerez@gmail.com>
2016-10-29 14:32:39 -0700

    build a temp headline

---
+++ app/assets/javascripts/miniProjects-animate.js
@@ -0,0 +1,9 @@
+$( function() {
+      $( '.anim-typewriter' ).teletype( {
+        text: [ 'My AMAZING list of CRAZY Projects', 'haha Na, Im just trying shit out' ],
+        typeDelay: 0,
+        backDelay: 0,
+        loop: 0,
+        cursor: '▋'
+      } );
+} );
--- app/assets/javascripts/miniProjects.js
+++ app/assets/javascripts/miniProjects.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
-//= require teletype.min
+//= require jquery.teletype.min
 //= require miniProjects-animate
