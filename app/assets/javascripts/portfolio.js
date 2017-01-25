2609732b4586297f6f95ce55541ba0b875625abb
Arturo Perez <VrturoPerez@gmail.com>
2017-01-25 15:13:23 -0800

    add counter js

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -19,4 +19,37 @@ $(document).ready(function(){
           scrollTop: $("#contact").offset().top
       }, 1500);
   });
+
+  /* _____________________________________
+
+     Counter
+     _____________________________________ */
+
+    if ($(".timer").length) {
+      var p = $('#counter'),
+          cc = 1;
+
+      $(window).on('scroll', function () {
+        var elemPos = p.offset().top,
+          elemPosBottom = p.offset().top + p.height(),
+          winHeight = $(window).height(),
+          scrollToElem = elemPos - winHeight,
+          winScrollTop = $(this).scrollTop();
+
+        if (winScrollTop > scrollToElem) {
+          if (elemPosBottom > winScrollTop) {
+            if (cc < 2) {
+              cc = 2;
+              $('.timer').countTo({
+                formatter: function (value, options) {
+                  value = value.toFixed(options.decimals);
+                  value = value.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+                  return value;
+                }
+              });
+            }
+          }
+        }
+      });
+    }
 })
